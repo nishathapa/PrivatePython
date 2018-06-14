3cb89854e9c33f9e80e5f966617cbe8f3a6e28b8
nisha_thapa <nishathapa4727@gmail.com>
2018-06-14 23:00:59 +0530

    Modified prime number Program

--- primenumber.py
+++ primenumber.py
@@ -1,5 +1,13 @@
-p=int(input(""))
-if p%1!=0 or p%i!=0:
-    print("not a prime number")
+# Python program to check if the input number is prime or not
+
+p=int(input())
+
+if p>1:
+    for i in range(2,p):
+        if p%i==0:
+            print(p,"not a prime number")
+            break
+    else:
+        print(p,"is a prime")
 else:
-    print("prime number")
+    print(p,"is a not a prime")
