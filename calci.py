cab448d2ed8945ca21f3dd8776998920ffa903fd
nisha_thapa <nishathapa4727@gmail.com>
2018-06-15 22:32:27 +0530

    Updated calculator program for multiple calculations

--- calci.py
+++ calci.py
@@ -1,15 +1,21 @@
 
-q=input("choose an operation: add,subtract,multiply,divide")
-print(q)
-print("first number:")
-a=int(input())
-print("second number:")
-b=int(input())
-if q=="add" :
-    print (a+b)
-elif q=="subtract":
-    print (a-b)
-elif q=="m  ultiply":
-    print (a*b)
-else:
-    print (a/b)
+#if user inputs y then repeat otherwise exit
+t=input("Do want to perform any calculation : (y/n)")
+while t == "y":
+    q = input("choose an operation: add,subtract,multiply,divide")
+    # print(q)
+    print("first number:")
+    a = int(input())
+    print("second number:")
+    b = int(input())
+    if q == "add":
+        print(a + b)
+    elif q == "subtract":
+        print(a - b)
+    elif q == "multiply":
+        print(a * b)
+    else:
+        print(a / b)
+    t = input("Do want to perform any calculation : (y/n)")
+print("thanks")
+
